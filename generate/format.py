7222ebbd88b06c239b02337c2b3640a8a2cbef5f
Caitlin Pratt <caitlin.c.pratt@gmail.com>
2019-06-20 17:15:15 -0400

    Initial commit

---
+++ generate/format.py
@@ -0,0 +1,123 @@
+import re
+
+
+class easyMail:
+    def __init__(self, template, uf_email_filepath, hero_image, preview_text, url, button=True):
+        self.template = template
+        self.uf_email_filepath = uf_email_filepath
+        self.hero_image = hero_image
+        self.preview_text = preview_text
+        self.url = url
+        self.button = button
+        self.f_copy = ''
+        self.f_hero = ''
+        self.f_body = ''
+        self.f_email = ''
+
+
+    @staticmethod
+    def load_template(template):
+        template = open(template, mode='r')
+        template = template.read()
+        return template
+
+    @staticmethod
+    def format_block(uf_block):
+        uf_block = open(uf_block, mode='r')
+        f_block = uf_block.read()
+        return f_block
+
+    def format_copy(self):
+        uf_copy = open(self.uf_email_filepath, mode='r')
+        f_copy = ''
+
+        for text in uf_copy:
+            if len(text) > 1 and re.search("(%%HERO%%)(.*?)(%%HERO%%)", text, re.S):
+                self.f_hero = re.search("(%%HERO%%)(.*?)(%%HERO%%)", text, re.S).group(2)
+                self.f_hero = (self.format_block('../templates/hero/header_p.txt')
+                               + self.f_hero + '</strong></p>')
+
+            elif len(text) > 1:
+                f_copy = f_copy + (self.format_block('../templates/hero/body_p.txt')
+                                   + text + '</span></p>')
+            self.f_copy = f_copy
+
+    def format_hero(self):
+        hero_uf = re.findall(str(self.format_block('../templates/hero/header_p.txt') + '.*?</p>'),
+                             self.f_hero, re.S)
+        hero_uf = hero_uf[-1]
+        hero_f = re.sub('margin-bottom:1rem', 'margin-bottom:0', hero_uf, re.S)
+        self.f_hero = re.sub(hero_uf, hero_f, self.f_hero, re.S)
+
+    def format_body(self):
+        self.f_body = re.sub("(%%HERO%%)(.*?)(%%HERO%%)", '', self.f_copy, re.S)
+        graphs_uf = re.findall(
+            str(self.format_block('../templates/hero/body_p.txt') + '.*?</p>'),
+            self.f_body, re.S)
+        graphs_uf = graphs_uf[-1]
+        graphs_f = re.sub('margin-bottom:1rem', 'margin-bottom:0', graphs_uf, re.S)
+        self.f_body = re.sub(graphs_uf, graphs_f, self.f_body, re.S)
+
+    def insert_into_template(self):
+        template = self.load_template(self.template)
+        self.f_email = template
+        self.f_email = re.sub("%%HERO_TEXT%%", self.f_hero, self.f_email)
+        self.f_email = re.sub("%%BODY_TEXT%%", self.f_body, self.f_email)
+        self.f_email = re.sub("%%HERO_BANNER_SOURCE%%", self.hero_image, self.f_email)
+        self.f_email = re.sub("%%PREVIEW_TEXT%%", self.preview_text, self.f_email)
+        if self.button is True:
+            bh = self.format_block('../templates/hero/button_header.txt')
+            print(bh)
+            bf = self.format_block('../templates/hero/button_footer.txt')
+            self.f_email = re.sub("%%BUTTON_HEADER%%", bh, self.f_email, re.S)
+            self.f_email = re.sub("%%BUTTON_FOOTER%%", bf, self.f_email, re.S)
+        elif self.button is False:
+            self.f_email = re.sub("%%BUTTON_HEADER%%", '', self.f_email, re.S)
+            self.f_email = re.sub("%%BUTTON_FOOTER%%", '', self.f_email, re.S)
+
+    def find_links(self):
+        a = self.format_block('../templates/hero/a.txt')
+
+        if len(re.findall('%%LINK%%', self.f_email)) % 2 != 0:
+            print(len(re.findall('%%LINK%%', self.f_email)))
+            print('Oops! Looks like you need to close a link somewhere in the text file.',
+                  'Make sure every link starts and ends with %%LINK%%!')
+            exit()
+
+        self.f_email = re.sub(r"(%%LINK%%)(.*?)(%%LINK%%)", (r"%s\2</a>" % a), self.f_email, re.S)
+
+    def find_salutation(self):
+        self.f_email = re.sub(r"%%SALUTATION%%", '{salutation~First Name or Friend}', self.f_email, re.S)
+
+    def find_bold(self):
+        self.f_email = re.sub(r"(%%BOLD%%)(.*?)(%%BOLD%%)", r"<strong>\2</strong>", self.f_email, re.S)
+
+    def find_italic(self):
+        self.f_email = re.sub(r"(%%ITALIC%%)(.*?)(%%ITALIC%%)", r"<em>\2</em>", self.f_email, re.S)
+
+    def find_underline(self):
+        self.f_email = re.sub(r"(%%UNDERLINE%%)(.*?)(%%UNDERLINE%%)",
+                              r"<span style='text-decoration:underline'>\2</span>", self.f_email, re.S)
+
+    def find_red(self):
+        self.f_email = re.sub(r"(%%RED%%)(.*?)(%%RED%%)", r"<span style='color:red'>\2</span>", self.f_email, re.S)
+
+    def insert_url(self):
+        self.f_email = re.sub(r"%%URL%%", self.url, self.f_email, re.S)
+
+    def create_email(self):
+        self.format_copy()
+        self.format_hero()
+        self.format_body()
+        self.insert_into_template()
+        self.find_links()
+        self.find_salutation()
+        self.find_bold()
+        self.find_italic()
+        self.find_underline()
+        self.find_red()
+        self.insert_url()
+
+        with open(self.uf_email_filepath[:-4] + '.html', "w") as f:
+            f.write(self.f_email)
+            f.close()
---
+++ templates/hero.py
@@ -0,0 +1,3 @@
+class hero():
+    def __init__(self):
+        self.paragraph = ['<p style="font-family:Arial, Helvetica, sans-serif;font-size:9px;line-height:1.4;margin-top:0;margin-bottom:0;text-align:center;color:#000;">', '</p>']
