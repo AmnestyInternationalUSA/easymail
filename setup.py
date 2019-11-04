0b02a22623573c22d14890aaa1950feabc3c5fa6
Caitlin Pratt <caitlin.c.pratt@gmail.com>
2019-11-04 11:59:51 -0500

    Created Tkinter UI

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='easymail',
+    version='',
+    packages=['generate', 'templates', 'templates.side-image'],
+    url='',
+    license='',
+    author='cpratt',
+    author_email='',
+    description=''
+)
